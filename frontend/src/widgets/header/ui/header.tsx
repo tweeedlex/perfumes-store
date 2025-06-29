647897f0d36315467d97e0a6da6f575a84dc3335
tweeedlex <7545245@ukr.net>
2025-06-29 17:11:19 +0300

    added logo

--- frontend/src/widgets/header/ui/header.tsx
+++ frontend/src/widgets/header/ui/header.tsx
@@ -2,22 +2,28 @@
 import React from 'react';
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import CartModal, {CartProvider, CartTrigger} from "@/widgets/cart-sidebar";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
     <>
-      <header className="flex justify-end items-center p-4 gap-4 h-16 relative z-10">
-        <SignedOut>
-          <SignInButton/>
-          <SignUpButton/>
-        </SignedOut>
-        <SignedIn>
-          <UserButton/>
-        </SignedIn>
-        <CartProvider>
-          <CartTrigger />
-          <CartModal />
-        </CartProvider>
+      <header className="p-4 flex items-center justify-between h-16 relative z-10">
+        <Link href="/">
+          Perfumes
+        </Link>
+        <div className={"flex justify-end items-center gap-4"}>
+          <SignedOut>
+            <SignInButton/>
+            <SignUpButton/>
+          </SignedOut>
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
+          <CartProvider>
+            <CartTrigger/>
+            <CartModal/>
+          </CartProvider>
+        </div>
       </header>
     </>
   );
