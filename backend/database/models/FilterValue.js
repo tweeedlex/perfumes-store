6629bff09d0c4e33172ff087504836ebf7fa7a73
tweeedlex <7545245@ukr.net>
2025-08-03 16:37:21 +0300

    fix

--- backend/database/models/FilterValue.js
+++ backend/database/models/FilterValue.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-  filterId: { type: mongoose.Schema.Types.ObjectId, ref: "Filter", required: true },
+  filter: { type: mongoose.Schema.Types.ObjectId, ref: "Filter", required: true },
   value: { type: String, required: true }
 });
 
-schema.index({ filterId: 1, value: 1 }, { unique: true });
+schema.index({ filter: 1, value: 1 }, { unique: true });
 
 module.exports = schema;
